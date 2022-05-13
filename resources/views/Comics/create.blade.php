fdeb5b2c1b8b1454d4713c1ca6ddc9d91f0253ab
MattFofo <matteo.segattini2@gmail.com>
2022-05-13 17:09:41 +0200

    fix description input

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -17,7 +17,7 @@
             @enderror
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <input type="text" class="form-control" id="description" name="description">
+                <textarea class="form-control" id="description" name="description">
             </div>
             @error('description')
                 <div class="alert alert-danger">{{ $message }}</div>
